353379cc17088b7a8492e9027961816d64b06782
lordslair <5469390+lordslair@users.noreply.github.com>
2020-04-25 15:40:47 +0200

    Fixed Typo

--- api/variables.py
+++ api/variables.py
@@ -3,10 +3,10 @@
 import os
 
 # JWT variables
-SEP_SECRET_KEY = eval(os.environ['SEP_SECRET_KEY'])
+SEP_SECRET_KEY = os.environ['SEP_SECRET_KEY']
 
 # MySQL variables
-MYSQL_USER     = eval(os.environ['SEP_MYSQL_USERY'])
-MYSQL_PASSWORD = eval(os.environ['SEP_MYSQL_PASSWORDY'])
-MYSQL_DB       = eval(os.environ['SEP_MYSQL_DBY'])
-MYSQL_HOST     = eval(os.environ['SEP_MYSQL_HOSTY'])
+MYSQL_USER     = os.environ['SEP_MYSQL_USER']
+MYSQL_PASSWORD = os.environ['SEP_MYSQL_PASSWORD']
+MYSQL_DB       = os.environ['SEP_MYSQL_DB']
+MYSQL_HOST     = os.environ['SEP_MYSQL_HOST']
