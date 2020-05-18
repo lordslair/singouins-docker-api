41246d3d31827bbee75f5653195ae26817674a1a
lordslair <5469390+lordslair@users.noreply.github.com>
2020-05-18 19:34:52 +0200

    Added tests for redis

---
+++ api/tests/test_02_redis.py
@@ -0,0 +1,33 @@
+# -*- coding: utf8 -*-
+
+import sys
+import os
+import redis
+import time
+
+code = os.path.dirname(os.environ['FLASK_APP'])
+sys.path.append(code)
+
+from app import app
+
+from utils.variables import (REDIS_PORT,
+                             REDIS_HOST,
+                             REDIS_DB_NAME)
+
+r = redis.StrictRedis(host     = REDIS_HOST,
+                      port     = REDIS_PORT,
+                      db       = REDIS_DB_NAME,
+                      encoding = 'utf-8')
+
+def test_singouins_redis_connection():
+    assert r.ping()
+
+def test_singouins_redis_set():
+    assert r.set('test_singouins', 'OK', ex = 1)
+
+def test_singouins_redis_get():
+    assert r.get('test_singouins')
+
+def test_singouins_redis_expired():
+    time.sleep(1)
+    assert not r.get('test_singouins')
