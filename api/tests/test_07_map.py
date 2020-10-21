b07fe89d95521b4a211578cc059783c5e5e52684
lordslair <5469390+lordslair@users.noreply.github.com>
2020-10-21 16:49:34 +0200

    Added map tests

---
+++ api/tests/test_07_map.py
@@ -0,0 +1,22 @@
+# -*- coding: utf8 -*-
+
+import json
+import os
+import requests
+
+SEP_URL     = os.environ['SEP_URL']
+mapid       = 1
+payload     = {'username': 'user', 'password': 'plop'}
+
+def test_singouins_map():
+    url      = SEP_URL + '/auth/login'
+    response = requests.post(url, json = payload)
+    token    = json.loads(response.text)['access_token']
+    headers  = json.loads('{"Authorization": "Bearer '+ token + '"}')
+
+
+    url      = SEP_URL + '/map/{}'.format(mapid)
+    response = requests.get(url, headers=headers)
+
+    assert response.status_code == 200
+    assert json.loads(response.text)['success'] == True
