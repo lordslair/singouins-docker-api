9a38c42019eeefe8727fb91c8c9ba047d90dd3da
lordslair <5469390+lordslair@users.noreply.github.com>
2020-10-21 16:49:42 +0200

    Added meta tests

---
+++ api/tests/test_08_meta.py
@@ -0,0 +1,23 @@
+# -*- coding: utf8 -*-
+
+import json
+import os
+import requests
+
+SEP_URL     = os.environ['SEP_URL']
+metalist    = ['gear','weapon']
+payload     = {'username': 'user', 'password': 'plop'}
+
+def test_singouins_meta():
+    url      = SEP_URL + '/auth/login'
+    response = requests.post(url, json = payload)
+    token    = json.loads(response.text)['access_token']
+    headers  = json.loads('{"Authorization": "Bearer '+ token + '"}')
+
+
+    for meta in metalist:
+        url      = SEP_URL + '/meta/item/{}'.format(meta)
+        response = requests.get(url, headers=headers)
+
+        assert response.status_code == 200
+        assert json.loads(response.text)['success'] == True
