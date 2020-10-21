84d35d778368efc705417a95c8258618a9c0804e
lordslair <5469390+lordslair@users.noreply.github.com>
2020-10-21 14:35:55 +0200

    Added more squad tests

--- api/tests/test_05_squad.py
+++ api/tests/test_05_squad.py
@@ -22,6 +22,7 @@ def test_singouins_squad_create():
     payload_s = {"name": 'SquadTest'}
     response  = requests.post(url, json = payload_s, headers=headers)
 
+    assert json.loads(response.text)['success'] == True
     assert response.status_code == 201
 
 def test_singouins_squad_get():
@@ -40,6 +41,53 @@ def test_singouins_squad_get():
     squad_r  = json.loads(response.text)['payload']['members'][0]['squad_rank']
 
     assert squad_r == 'Leader'
+    assert json.loads(response.text)['success'] == True
+    assert response.status_code == 200
+
+def test_singouins_squad_invite_and_kick():
+    url      = SEP_URL + '/auth/login'
+    response = requests.post(url, json = payload)
+    token    = json.loads(response.text)['access_token']
+    headers  = json.loads('{"Authorization": "Bearer '+ token + '"}')
+
+    url      = SEP_URL + '/mypc'
+    response = requests.get(url, headers=headers)
+    pjid     = json.loads(response.text)['payload'][0]['id']
+    squadid  = json.loads(response.text)['payload'][0]['squad']
+
+    url      = SEP_URL + '/mypc/{}/squad/{}/invite/1'.format(pjid,squadid)
+    response = requests.post(url, headers=headers)
+
+    assert json.loads(response.text)['success'] == False
+    assert 'already in a squad' in json.loads(response.text)['msg']
+    assert response.status_code == 200
+
+    # We create a PJTestSquad just to invite/kick
+    url      = SEP_URL + '/mypc'
+    response = requests.post(url, json = {'race': '3', 'name': 'PJTestSquad'}, headers=headers)
+    url      = SEP_URL + '/mypc'
+    response = requests.get(url, headers=headers)
+    targetid = json.loads(response.text)['payload'][1]['id']
+
+    url      = SEP_URL + '/mypc/{}/squad/{}/invite/{}'.format(pjid,squadid,targetid)
+    response = requests.post(url, headers=headers)
+
+    assert json.loads(response.text)['success'] == True
+    assert 'PC successfully invited' in json.loads(response.text)['msg']
+    assert response.status_code == 201
+
+    url      = SEP_URL + '/mypc/{}/squad/{}/kick/{}'.format(pjid,squadid,targetid)
+    response = requests.post(url, headers=headers)
+
+    assert json.loads(response.text)['success'] == True
+    assert 'PC successfully kicked' in json.loads(response.text)['msg']
+    assert response.status_code == 200
+
+    # We cleanup the PJTestSquad
+    url      = SEP_URL + '/mypc/{}'.format(targetid)
+    response = requests.delete(url, headers=headers)
+
+    assert json.loads(response.text)['success'] == True
     assert response.status_code == 200
 
 def test_singouins_squad_delete():
@@ -56,4 +104,5 @@ def test_singouins_squad_delete():
     url      = SEP_URL + '/mypc/{}/squad/{}'.format(pjid,squadid)
     response = requests.delete(url, headers=headers)
 
+    assert json.loads(response.text)['success'] == True
     assert response.status_code == 200
