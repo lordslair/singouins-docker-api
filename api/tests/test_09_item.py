0366270c44fd4e8e38666668782a2d4c141850c9
lordslair <5469390+lordslair@users.noreply.github.com>
2020-10-21 16:49:49 +0200

    Added item tests

---
+++ api/tests/test_09_item.py
@@ -0,0 +1,25 @@
+# -*- coding: utf8 -*-
+
+import json
+import os
+import requests
+
+SEP_URL     = os.environ['SEP_URL']
+pjname_test = 'PJTest'
+payload     = {'username': 'user', 'password': 'plop'}
+
+def test_singouins_item():
+    url      = SEP_URL + '/auth/login'
+    response = requests.post(url, json = payload)
+    token    = json.loads(response.text)['access_token']
+    headers  = json.loads('{"Authorization": "Bearer '+ token + '"}')
+
+    url      = SEP_URL + '/mypc'
+    response = requests.get(url, headers=headers)
+    pjid     = json.loads(response.text)['payload'][0]['id']
+
+    url       = SEP_URL + '/mypc/{}/item'.format(pjid)
+    response  = requests.get(url, headers=headers)
+
+    assert json.loads(response.text)['success'] == True
+    assert response.status_code == 200
