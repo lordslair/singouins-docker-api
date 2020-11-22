9df71486116a92ae4fa3e12e3abddc4c03990b6d
lordslair <5469390+lordslair@users.noreply.github.com>
2020-11-22 23:23:48 +0100

    Refactored auth()

--- api/queries/sql/mysql.py
+++ api/queries/sql/mysql.py
@@ -45,31 +45,49 @@ def get_username_exists(username):
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    with engine.connect() as conn:
-        result = session.query(User).filter(User.name == username).one_or_none()
+    try:
+        result = session.query(User)\
+                        .filter(User.name == username)\
+                        .one_or_none()
+    except Exception as e:
+        # Something went wrong during query
+        return False
+    else:
+        if result: return True
+    finally:
         session.close()
-
-    if result: return True
 
 def get_usermail_exists(usermail):
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    with engine.connect() as conn:
-       result = session.query(User).filter(User.mail == usermail).one_or_none()
-       session.close()
-
-    if result: return True
+    try:
+       result = session.query(User)\
+                       .filter(User.mail == usermail)\
+                       .one_or_none()
+    except Exception as e:
+        # Something went wrong during query
+        return False
+    else:
+        if result: return True
+    finally:
+        session.close()
 
 def get_user(username):
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    with engine.connect() as conn:
-       result = session.query(User).filter(User.name == username).one_or_none()
-       session.close()
-
-    if result: return result
+    try:
+       result = session.query(User)\
+                       .filter(User.name == username)\
+                       .one_or_none()
+    except Exception as e:
+        # Something went wrong during query
+        return False
+    else:
+        if result: return result
+    finally:
+        session.close()
 
 def add_user(username,password,usermail):
     if get_username_exists(username) or get_usermail_exists(usermail):
@@ -80,40 +98,44 @@ def add_user(username,password,usermail):
         Session = sessionmaker(bind=engine)
         session = Session()
 
-        with engine.connect() as conn:
-            user = User(name      = username,
-                        mail      = usermail,
-                        hash      = generate_password_hash(password, rounds = 10),
-                        d_name    = '',
-                        d_monkeys = '', # Todo later
-                        d_ack     = False,
-                        created   = datetime.now(),
-                        active    = True)
+        user = User(name      = username,
+                    mail      = usermail,
+                    hash      = generate_password_hash(password, rounds = 10),
+                    d_name    = '',
+                    d_monkeys = '', # Todo later
+                    d_ack     = False,
+                    created   = datetime.now(),
+                    active    = True)
 
-            session.add(user)
+        session.add(user)
 
-            try:
-                session.commit()
-            except Exception as e:
-                # Something went wrong during commit
-                return (422)
-            else:
-                return (201)
-
-def set_user_confirmed(usermail):
-    Session = sessionmaker(bind=engine)
-    session = Session()
-
-    with engine.connect() as conn:
         try:
-            user = session.query(User).filter(User.mail == usermail).one_or_none()
-            user.active = True
             session.commit()
         except Exception as e:
             # Something went wrong during commit
             return (422)
         else:
             return (201)
+        finally:
+            session.close()
+
+def set_user_confirmed(usermail):
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    try:
+        user = session.query(User)\
+                      .filter(User.mail == usermail)\
+                      .one_or_none()
+        user.active = True
+        session.commit()
+    except Exception as e:
+        # Something went wrong during commit
+        return (422)
+    else:
+        return (201)
+    finally:
+        session.close()
 
 def del_user(username):
     if not get_username_exists(username):
@@ -122,16 +144,19 @@ def del_user(username):
         Session = sessionmaker(bind=engine)
         session = Session()
 
-        with engine.connect() as conn:
-            try:
-                user = session.query(User).filter(User.name == username).one_or_none()
-                session.delete(user)
-                session.commit()
-            except Exception as e:
-                # Something went wrong during commit
-                return (422)
-            else:
-                return (200)
+        try:
+            user = session.query(User)\
+                          .filter(User.name == username)\
+                          .one_or_none()
+            session.delete(user)
+            session.commit()
+        except Exception as e:
+            # Something went wrong during commit
+            return (422)
+        else:
+            return (200)
+        finally:
+            session.close()
 
 #
 # Queries: /pc
