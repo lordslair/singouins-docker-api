97fd759649def2ec7c8639fce57fbe3a21bb2a08
lordslair <5469390+lordslair@users.noreply.github.com>
2020-11-27 15:07:36 +0100

    Added logging

--- api/mysql/utils/initialize.py
+++ api/mysql/utils/initialize.py
@@ -4,4 +4,6 @@ from ..engine                   import engine
 from ..base                     import Base
 
 def initialize_db():
+    print('DB init: start')
     Base.metadata.create_all(bind=engine)
+    print('DB init: end')
