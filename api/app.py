435437d2e2fd0eed16fe0fbaf42014453495786c
lordslair <5469390+lordslair@users.noreply.github.com>
2020-11-30 16:40:17 +0100

    moved /dismantle to /action

--- api/app.py
+++ api/app.py
@@ -285,13 +285,6 @@ def api_mypc_item_get(pcid):
     if isinstance(code, int):
         return jsonify({"msg": msg, "success": success, "payload": payload}), code
 
-@app.route('/mypc/<int:pcid>/item/<int:itemid>/dismantle', methods=['POST'])
-@jwt_required
-def api_mypc_item_dismantle(pcid,itemid):
-    (code, success, msg, payload) = mypc_item_dismantle(get_jwt_identity(),pcid,itemid)
-    if isinstance(code, int):
-        return jsonify({"msg": msg, "success": success, "payload": payload}), code
-
 @app.route('/mypc/<int:pcid>/item/<int:itemid>/inventory/offset/<int:offsetx>/<int:offsety>', methods=['POST'])
 @jwt_required
 def mypc_item_set_offset(pcid,itemid,offsetx,offsety):
@@ -325,6 +318,13 @@ def mypc_action_move_path(pcid):
 @jwt_required
 def api_mypc_action_item_give(pcid,itemid,targetid):
     (code, success, msg, payload) = mypc_action_item_give(get_jwt_identity(),pcid,itemid,targetid)
+    if isinstance(code, int):
+        return jsonify({"msg": msg, "success": success, "payload": payload}), code
+
+@app.route('/mypc/<int:pcid>/action/item/<int:itemid>/dismantle', methods=['POST'])
+@jwt_required
+def api_mypc_action_item_dismantle(pcid,itemid):
+    (code, success, msg, payload) = mypc_action_item_dismantle(get_jwt_identity(),pcid,itemid)
     if isinstance(code, int):
         return jsonify({"msg": msg, "success": success, "payload": payload}), code
 
--- api/mysql/methods/mypc_action.py
+++ api/mysql/methods/mypc_action.py
@@ -69,3 +69,69 @@ def mypc_action_item_give(username,pcid,itemid,targetid):
                 item)
     finally:
         session.close()
+
+def mypc_action_item_dismantle(username,pcid,itemid):
+    (code, success, msg, pc) = fn_creature_get(None,pcid)
+    user                     = fn_user_get(username)
+    session                  = Session()
+
+    if pc and pc.account != user.id:
+        return (409, False, 'Token/username mismatch', None)
+
+    item = session.query(Item)\
+                  .filter(Item.id == itemid)\
+                  .one_or_none()
+
+    if item is None:
+        # Item not found
+        return (200,
+                False,
+                'Item not found (pcid:{},itemid:{})'.format(pcid,itemid),
+                None)
+
+    if   item.rarity == 'Broken':
+        shards = [6,0,0,0,0,0]
+    elif item.rarity == 'Common':
+        shards = [0,5,0,0,0,0]
+    elif item.rarity == 'Uncommon':
+        shards = [0,0,4,0,0,0]
+    elif item.rarity == 'Rare':
+        shards = [0,0,0,3,0,0]
+    elif item.rarity == 'Epic':
+        shards = [0,0,0,0,2,0]
+    elif item.rarity == 'Legendary':
+        shards = [0,0,0,0,0,1]
+
+    try:
+        wallet = session.query(Wallet).filter(Wallet.id == pc.id).one_or_none()
+
+        wallet.broken    += shards[0]
+        wallet.common    += shards[1]
+        wallet.uncommon  += shards[2]
+        wallet.rare      += shards[3]
+        wallet.epic      += shards[4]
+        wallet.legendary += shards[5]
+
+        session.delete(item)
+
+        session.commit()
+    except Exception as e:
+        # Something went wrong during query
+        return (200,
+                False,
+                '[SQL] Wallet update failed (pcid:{})'.format(pc.id),
+                None)
+    else:
+        return (200,
+                True,
+                'Item dismantle successed (pcid:{},itemid:{})'.format(pc.id,item.id),
+                {"shards": {
+                    "Broken":    shards[0],
+                    "Common":    shards[1],
+                    "Uncommon":  shards[2],
+                    "Rare":      shards[3],
+                    "Epic":      shards[4],
+                    "Legendary": shards[5]}
+                })
+    finally:
+        session.close()
