5875440253a7584e75c190482f6ee9589a6a3452
lordslair <5469390+lordslair@users.noreply.github.com>
2020-12-03 23:25:06 +0100

    Added pool_pre_ping

--- api/mysql/engine.py
+++ api/mysql/engine.py
@@ -4,4 +4,6 @@ from sqlalchemy     import create_engine
 
 from variables      import SQL_DSN
 
-engine = create_engine('mysql+pymysql://' + SQL_DSN, pool_recycle=3600)
+engine = create_engine('mysql+pymysql://' + SQL_DSN,
+                       pool_recycle=3600,
+                       pool_pre_ping=True)
