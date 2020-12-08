17cde038179ac271c51fc3482e54f1d90fc3e183
lordslair <5469390+lordslair@users.noreply.github.com>
2020-12-08 11:49:07 +0100

    Moved to singouins-k8s repo

--- k8s/nginx/certbot-init.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-certbot certonly --logs-dir /var/www/certbot \
-                 --webroot --webroot-path /var/www/certbot \
-                 -d <CERTBOT_DOMAINS> \
-                 --email <CERTBOT_EMAIL> \
-                 --agree-tos \
-                 --no-eff-email
