703696e8dedd6c81999f4bd59297a41ba99b4be2
lordslair <5469390+lordslair@users.noreply.github.com>
2022-01-21 16:03:32 +0100

    Added /internal tests

--- api/tests/internal/test_02_internal_creature.py
+++ api/tests/internal/test_02_internal_creature.py
@@ -3,7 +3,26 @@
 import json
 import requests
 
-from variables import *
+from variables import (API_INTERNAL_TOKEN,
+                       API_URL)
+
+def test_singouins_internal_creature_cds():
+    url       = API_URL + '/internal/creature/cds'
+    payload   = {"creatureid": 1}
+    headers   = json.loads('{"Authorization": "Bearer '+ API_INTERNAL_TOKEN + '"}')
+    response  = requests.post(url, headers=headers, json = payload)
+
+    assert response.status_code                 == 200
+    assert json.loads(response.text)['success'] == True
+
+def test_singouins_internal_creature_effects():
+    url       = API_URL + '/internal/creature/effects'
+    payload   = {"creatureid": 1}
+    headers   = json.loads('{"Authorization": "Bearer '+ API_INTERNAL_TOKEN + '"}')
+    response  = requests.post(url, headers=headers, json = payload)
+
+    assert response.status_code                 == 200
+    assert json.loads(response.text)['success'] == True
 
 def test_singouins_internal_creature_equipment():
     url       = API_URL + '/internal/creature/equipment'
@@ -11,4 +30,59 @@ def test_singouins_internal_creature_equipment():
     headers   = json.loads('{"Authorization": "Bearer '+ API_INTERNAL_TOKEN + '"}')
     response  = requests.post(url, headers=headers, json = payload)
 
-    assert response.status_code == 200
+    assert response.status_code                 == 200
+    assert json.loads(response.text)['success'] == True
+
+def test_singouins_internal_creature_pa():
+    url       = API_URL + '/internal/creature/pa'
+    payload   = {"creatureid": 1}
+    headers   = json.loads('{"Authorization": "Bearer '+ API_INTERNAL_TOKEN + '"}')
+    response  = requests.post(url, headers=headers, json = payload)
+
+    assert response.status_code                 == 200
+    assert json.loads(response.text)['success'] == True
+
+def test_singouins_internal_creature_pa_reset():
+    url       = API_URL + '/internal/creature/pa/reset'
+    payload   = {"creatureid": 1}
+    headers   = json.loads('{"Authorization": "Bearer '+ API_INTERNAL_TOKEN + '"}')
+    response  = requests.post(url, headers=headers, json = payload)
+
+    assert response.status_code                 == 200
+    assert json.loads(response.text)['success'] == True
+
+def test_singouins_internal_creature_profile():
+    url       = API_URL + '/internal/creature/profile'
+    payload   = {"creatureid": 1}
+    headers   = json.loads('{"Authorization": "Bearer '+ API_INTERNAL_TOKEN + '"}')
+    response  = requests.post(url, headers=headers, json = payload)
+
+    assert response.status_code                 == 200
+    assert json.loads(response.text)['success'] == True
+
+def test_singouins_internal_creature_stats():
+    url       = API_URL + '/internal/creature/stats'
+    payload   = {"creatureid": 1}
+    headers   = json.loads('{"Authorization": "Bearer '+ API_INTERNAL_TOKEN + '"}')
+    response  = requests.post(url, headers=headers, json = payload)
+
+    assert response.status_code                 == 200
+    assert json.loads(response.text)['success'] == True
+
+def test_singouins_internal_creature_statuses():
+    url       = API_URL + '/internal/creature/statuses'
+    payload   = {"creatureid": 1}
+    headers   = json.loads('{"Authorization": "Bearer '+ API_INTERNAL_TOKEN + '"}')
+    response  = requests.post(url, headers=headers, json = payload)
+
+    assert response.status_code                 == 200
+    assert json.loads(response.text)['success'] == True
+
+def test_singouins_internal_creature_wallet():
+    url       = API_URL + '/internal/creature/wallet'
+    payload   = {"creatureid": 1}
+    headers   = json.loads('{"Authorization": "Bearer '+ API_INTERNAL_TOKEN + '"}')
+    response  = requests.post(url, headers=headers, json = payload)
+
+    assert response.status_code                 == 200
+    assert json.loads(response.text)['success'] == True
