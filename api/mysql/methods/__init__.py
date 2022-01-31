4dcc4be733c139e31836ab2662b9b7b9a28e34f2
lordslair <5469390+lordslair@users.noreply.github.com>
2022-01-31 18:06:33 +0100

    Fixed missing import

--- api/mysql/methods/__init__.py
+++ api/mysql/methods/__init__.py
@@ -15,6 +15,7 @@ from .mypc_korp         import *
 from .mypc_inventory    import *
 from .mypc_instance     import *
 from .mypc_item         import *
+from .mypc_map          import *
 from .mypc_mp           import *
 from .mypc_trade        import *
 from .mypc_squad        import *
