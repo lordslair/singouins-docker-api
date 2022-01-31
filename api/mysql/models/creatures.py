57fb9a9d93a68fb503d1c98c76919adce4e820ac
lordslair <5469390+lordslair@users.noreply.github.com>
2022-01-31 12:41:01 +0100

    Added new Creature SQL Class

--- api/mysql/models/creatures.py
+++ api/mysql/models/creatures.py
@@ -4,9 +4,11 @@ from sqlalchemy                 import Column, Integer, DateTime, Boolean, Text,
 from sqlalchemy                 import func
 
 from dataclasses                import dataclass
+from datetime                   import datetime
 
 from ..base                     import Base
 
+# DEPRECATED
 @dataclass
 class PJ(Base):
     __tablename__ = 'creatures'
@@ -69,6 +71,55 @@ class PJ(Base):
     p            = Column(Integer, nullable=False, server_default='0')
     b            = Column(Integer, nullable=False, server_default='0')
     date         = Column(DateTime(timezone=True), nullable=False, server_default=func.now(), server_onupdate=func.now())
+
+# New Creature class.
+# It will slowly replace in calls the former PJ class
+@dataclass
+class Creature(Base):
+    __tablename__ = 'creatures'
+    __table_args__ = {'extend_existing': True}
+
+    def __hash__(self):        # Used to aggregate & dedup lists of Creatures
+        return hash(self.name) # ex: with set() in /view
+
+    id:          int
+    name:        str
+    race:        int
+    gender:      bool
+    rarity:      str
+    account:     int
+    targeted_by: int
+    level:       int
+    x:           int
+    y:           int
+    xp:          int
+    instance:    int
+    squad:       int
+    squad_rank:  str
+    korp:        int
+    korp_rank:   str
+    date:        str
+
+    id           = Column(Integer, primary_key=True)
+    name         = Column(Text   , nullable=False)
+    race         = Column(Integer, nullable=False)
+    gender       = Column(Boolean, nullable=True, server_default='1')
+    rarity       = Column(Enum('Small','Medium','Big','Unique','Boss','God'), default='Medium')
+    account      = Column(Integer, nullable=True)
+    targeted_by  = Column(Integer, nullable=True)
+    level        = Column(Integer, nullable=False, server_default='1')
+    x            = Column(Integer, nullable=False, server_default='0')
+    y            = Column(Integer, nullable=False, server_default='0')
+    xp           = Column(Integer, nullable=False, server_default='0')
+    instance     = Column(Integer, nullable=True)
+    squad        = Column(Integer, nullable=True)
+    squad_rank   = Column(Enum('Leader','Member','Pending'), nullable=True)
+    korp         = Column(Integer, nullable=True)
+    korp_rank    = Column(Enum('Leader','Member','Pending'), nullable=True)
+    date         = Column(DateTime(timezone=True),
+                          nullable=False,
+                          server_default=datetime.now().isoformat(),
+                          server_onupdate=datetime.now().isoformat())
 
 @dataclass
 class Wallet(Base):
