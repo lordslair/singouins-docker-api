ac7e4bbda4873b58efac316d2c3965585f055e2f
lordslair <5469390+lordslair@users.noreply.github.com>
2022-01-31 17:47:00 +0100

    Added more metas to Redis

--- api/mysql/utils/redis/variables.py
+++ api/mysql/utils/redis/variables.py
@@ -16,6 +16,8 @@ REDIS_PORT    = os.environ['SEP_BACKEND_REDIS_SVC_SERVICE_PORT']
 REDIS_DB_NAME = os.environ['SEP_REDIS_DB']
 
 # Metafiles location for Redis init
-META_FILES = {'races':   'data/metas/races.json',
+META_FILES = {'armors':  'data/metas/armors.json',
+              'effects': 'data/metas/effects.json',
+              'races':   'data/metas/races.json',
               'skills':  'data/metas/skills.json',
-              'effects': 'data/metas/effects.json'}
+              'weapons': 'data/metas/weapons.json'}
