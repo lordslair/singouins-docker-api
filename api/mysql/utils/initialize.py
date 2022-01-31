e1380a10b36d3cb6fa3f998c7ec6ae69d66a2495
lordslair <5469390+lordslair@users.noreply.github.com>
2022-01-31 15:38:56 +0100

    Modified to load metas into Redis on startup

--- api/mysql/utils/initialize.py
+++ api/mysql/utils/initialize.py
@@ -6,16 +6,40 @@ from ..engine                   import engine
 from ..base                     import Base
 
 from .redis.connector           import r
+from .redis.variables           import META_FILES
 
 def initialize_db():
     print('DB init: start')
-    Base.metadata.create_all(bind=engine)
+
+    try:
+        Base.metadata.create_all(bind=engine)
+    except Exception as e:
+        print(f'DB init: failed [{e}]')
+    else:
+        print('DB init: done')
+
     print('DB init: end')
 
 def initialize_redis():
-    print('Redis init: start')
+    print(f'Redis init: start')
+
     try:
-        r.set('system:startup',datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
+        r.set('system:startup',datetime.now().isoformat())
+
     except Exception as e:
-        print(f'Redis init: failed {e}')
+        print(f'Redis init: failed [{e}]')
+    else:
+        print('Redis init: created system:startup')
+
+    try:
+        for meta,file in META_FILES.items():
+            with open(file) as f:
+                content = f.read()
+                print(f'Redis init: creating system:meta:{meta}')
+                r.set(f'system:meta:{meta}', content)
+    except Exception as e:
+        print(f'Redis init: failed [{e}]')
+    else:
+        print(f'Redis init: created system:meta:*')
+
     print('Redis init: end')
--- api/mysql/utils/redis/variables.py
+++ api/mysql/utils/redis/variables.py
@@ -14,3 +14,8 @@ bluemaxttl     = bluepaduration * bluepamax
 REDIS_HOST    = os.environ['SEP_BACKEND_REDIS_SVC_SERVICE_HOST']
 REDIS_PORT    = os.environ['SEP_BACKEND_REDIS_SVC_SERVICE_PORT']
 REDIS_DB_NAME = os.environ['SEP_REDIS_DB']
+
+# Metafiles location for Redis init
+META_FILES = {'races':   'data/metas/races.json',
+              'skills':  'data/metas/skills.json',
+              'effects': 'data/metas/effects.json'}
