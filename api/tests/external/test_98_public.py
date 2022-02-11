036975fb27e4e42f98ee00f1c3a3399e4de1141f
lordslair <5469390+lordslair@users.noreply.github.com>
2022-02-11 16:26:10 +0100

    Reworked semi-public /pc calls

---
+++ api/tests/external/test_98_public.py
@@ -0,0 +1,47 @@
+# -*- coding: utf8 -*-
+
+import json
+import requests
+
+from variables import (API_URL,
+                       AUTH_PAYLOAD,
+                       CREATURE_ID)
+
+def test_singouins_public_pc():
+    url      = f'{API_URL}/auth/login' # POST
+    response = requests.post(url, json = AUTH_PAYLOAD)
+    token    = json.loads(response.text)['access_token']
+    headers  = {"Authorization": f"Bearer {token}"}
+
+    pcid      = CREATURE_ID
+    url       = f'{API_URL}/pc/{pcid}' # GET
+    response  = requests.get(url, headers=headers)
+
+    assert json.loads(response.text)['success'] == True
+    assert response.status_code == 200
+
+def test_singouins_public_pc_events():
+    url      = f'{API_URL}/auth/login' # POST
+    response = requests.post(url, json = AUTH_PAYLOAD)
+    token    = json.loads(response.text)['access_token']
+    headers  = {"Authorization": f"Bearer {token}"}
+
+    pcid      = CREATURE_ID
+    url       = f'{API_URL}/pc/{pcid}/event' # GET
+    response  = requests.get(url, headers=headers)
+
+    assert json.loads(response.text)['success'] == True
+    assert response.status_code == 200
+
+def test_singouins_public_pc_item():
+    url      = f'{API_URL}/auth/login' # POST
+    response = requests.post(url, json = AUTH_PAYLOAD)
+    token    = json.loads(response.text)['access_token']
+    headers  = {"Authorization": f"Bearer {token}"}
+
+    pcid      = CREATURE_ID
+    url       = f'{API_URL}/pc/{pcid}/item' # GET
+    response  = requests.get(url, headers=headers)
+
+    assert json.loads(response.text)['success'] == True
+    assert response.status_code == 200
--- api/tests/public/variables.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf8 -*-
-
-import os
-
-API_PORT           = os.environ['GUNICORN_PORT']
-API_URL            = f'http://127.0.0.1:{API_PORT}'
-
-CREATURE_ID        = 1
