160688b28640059e53bf630618fd34e52dc14354
lordslair <5469390+lordslair@users.noreply.github.com>
2022-02-11 16:44:53 +0100

    Added /mypc tests

--- api/tests/external/test_03_pc.py
+++ api/tests/external/test_03_pc.py
@@ -57,3 +57,79 @@ def test_singouins_pc_view():
 
     assert response.status_code == 200
     assert json.loads(response.text)['success'] == True
+
+def test_singouins_pc_stats():
+    url      = f'{API_URL}/auth/login' # POST
+    response = requests.post(url, json = AUTH_PAYLOAD)
+    token    = json.loads(response.text)['access_token']
+    headers  = {"Authorization": f"Bearer {token}"}
+
+    url      = f'{API_URL}/mypc' # GET
+    response = requests.get(url, headers=headers)
+    pcid     = json.loads(response.text)['payload'][0]['id']
+
+    assert response.status_code == 200
+    assert json.loads(response.text)['success'] == True
+
+    url      = f'{API_URL}/mypc/{pcid}/stats' # GET
+    response = requests.get(url, headers=headers)
+
+    assert response.status_code == 200
+    assert json.loads(response.text)['success'] == True
+
+def test_singouins_pc_effects():
+    url      = f'{API_URL}/auth/login' # POST
+    response = requests.post(url, json = AUTH_PAYLOAD)
+    token    = json.loads(response.text)['access_token']
+    headers  = {"Authorization": f"Bearer {token}"}
+
+    url      = f'{API_URL}/mypc' # GET
+    response = requests.get(url, headers=headers)
+    pcid     = json.loads(response.text)['payload'][0]['id']
+
+    assert response.status_code == 200
+    assert json.loads(response.text)['success'] == True
+
+    url      = f'{API_URL}/mypc/{pcid}/effects' # GET
+    response = requests.get(url, headers=headers)
+
+    assert response.status_code == 200
+    assert json.loads(response.text)['success'] == True
+
+def test_singouins_pc_cds():
+    url      = f'{API_URL}/auth/login' # POST
+    response = requests.post(url, json = AUTH_PAYLOAD)
+    token    = json.loads(response.text)['access_token']
+    headers  = {"Authorization": f"Bearer {token}"}
+
+    url      = f'{API_URL}/mypc' # GET
+    response = requests.get(url, headers=headers)
+    pcid     = json.loads(response.text)['payload'][0]['id']
+
+    assert response.status_code == 200
+    assert json.loads(response.text)['success'] == True
+
+    url      = f'{API_URL}/mypc/{pcid}/cds' # GET
+    response = requests.get(url, headers=headers)
+
+    assert response.status_code == 200
+    assert json.loads(response.text)['success'] == True
+
+def test_singouins_pc_skills():
+    url      = f'{API_URL}/auth/login' # POST
+    response = requests.post(url, json = AUTH_PAYLOAD)
+    token    = json.loads(response.text)['access_token']
+    headers  = {"Authorization": f"Bearer {token}"}
+
+    url      = f'{API_URL}/mypc' # GET
+    response = requests.get(url, headers=headers)
+    pcid     = json.loads(response.text)['payload'][0]['id']
+
+    assert response.status_code == 200
+    assert json.loads(response.text)['success'] == True
+
+    url      = f'{API_URL}/mypc/{pcid}/skills' # GET
+    response = requests.get(url, headers=headers)
+
+    assert response.status_code == 200
+    assert json.loads(response.text)['success'] == True
