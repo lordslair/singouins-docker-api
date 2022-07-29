685b122c8d0753913cb5256c9e3bcc873f8f702b
lordslair <5469390+lordslair@users.noreply.github.com>
2022-07-29 23:10:08 +0200

    Fixed deprecated import

--- api/nosql/__init__.py
+++ api/nosql/__init__.py
@@ -7,7 +7,6 @@ from .initialize                import initialize_redis
 initialize_redis()
 
 from .incr                      import *
-from .instances                 import *
 from .maps                      import *
 from .metas                     import *
 from .queue                     import *
