5871c68bf67a86bdad60c8d74d76b21153518377
lordslair <5469390+lordslair@users.noreply.github.com>
2022-09-07 14:48:04 +0200

    Simplified RedisEvent usage

--- api/nosql/models/RedisEvent.py
+++ api/nosql/models/RedisEvent.py
@@ -2,80 +2,96 @@
 
 import time
 
-from datetime import datetime
+from datetime                   import datetime
+from loguru                     import logger
 
-from nosql.connector            import *
+from nosql.connector            import r
+
 
 class RedisEvent:
-    def __init__(self,creature):
-        RedisEvent.creature = creature
-        RedisEvent.logh     = f'[creature.id:{RedisEvent.creature.id}]'
+    def __init__(self, creature):
+        self.creature = creature
+        self.logh     = f'[Creature.id:{self.creature.id}]'
+        self.hkey     = f'events:{self.creature.id}'
+        logger.trace(f'{self.logh} Method >> Initialization')
 
-    @classmethod
-    def get(cls):
-        mypattern = f'events:{RedisEvent.creature.id}'
-        path      = f'{mypattern}:*'
+    def get(self):
+        path      = f'{self.hkey}:*'
         #                         └────> Wildcard for {ts}
         events    = []
 
         try:
+            logger.trace(f'{self.logh} Method >> (HASH Loading)')
             # We get the list of keys for all the events
             keys               = r.keys(path)
             sorted_keys        = sorted(keys)
-            # We MULTI the query to have all the values
-            multi              = r.mget(sorted_keys)
-            # We initialize indexes used during iterations
-            index_multi        = 0
             # We loop over the events keys to build the data
             for key in sorted_keys:
-                m = re.match(f'events:(\d+|None):(\d+):(\d+|None):(\w+)', key)
-                #                         │       │        │       └────> Regex for {type}
-                #                         │       │        └────────────> Regex for {dst}
-                #                         │       └─────────────────────> Regex for {ts}
-                #                         └─────────────────────────────> Regex for {src}
-                if m:
-                    # None (string) to Pythonic None conversion
-                    if m.group(1) == 'None':
-                        src = None
-                    else:
-                        src = int(m.group(1))
-                    if m.group(3) == 'None':
-                        dst = None
-                    else:
-                        dst = int(m.group(3))
-                    # We build the event item
-                    event = {"src":           src,
-                             "action":        multi[index_multi],
-                             "date":          datetime.fromtimestamp(int(m.group(2))//1000),
-                             "dst":           dst,
-                             "id":            index_multi+1,
-                             "type":          m.group(4)}
-                    # We update the index for next iteration
-                    index_multi    += 1
-                    # We add the event into events list
-                    events.append(event)
+                hashdict = r.hgetall(key)
+                # We need to convert empty strings into None for Redis
+                if hashdict['src'] == '':
+                    src = None
+                else:
+                    src = int(hashdict['src'])
+                if hashdict['dst'] == '':
+                    dst = None
+                else:
+                    dst = int(hashdict['dst'])
+                # We build the event item
+                event = {
+                    "src": src,
+                    "action": hashdict['action'],
+                    "date":
+                        datetime.fromtimestamp(int(hashdict['ts']) // 1000),
+                    "dst": dst,
+                    "id": sorted_keys.index(key) + 1,
+                    "type": hashdict['type'],
+                }
+                # We add the event into events list
+                events.append(event)
         except Exception as e:
-            logger.error(f'{RedisEvent.logh} Method KO [{e}]')
+            logger.error(f'{self.logh} Method KO [{e}]')
             return None
         else:
-            logger.trace(f'{RedisEvent.logh} Method OK')
+            logger.trace(f'{self.logh} Method OK')
             return events
 
-
-    @classmethod
-    def add(cls,src,dst,type,msg,ttl=None):
-
-        ts  = time.time_ns() // 1000000 # Time in milliseconds
-        key = f'events:{src}:{ts}:{dst}:{type}'
+    def add(self, src, dst, type, action, ttl=None):
+        ts  = time.time_ns() // 1000000  # Time in milliseconds
 
         try:
+            logger.trace(f'{self.logh} Method >> (HASH Creating)')
+            # We need to convert None into empty strings for Redis
+            if src is None:
+                src = ''
+            if dst is None:
+                dst = ''
+            hashdict = {
+                "action": action,
+                "dst": dst,
+                "src": src,
+                "ts": ts,
+                "type": type,
+            }
             if ttl is None:
-                r.set(key, msg)
+                r.hset(f'{self.hkey}:{ts}', mapping=hashdict)
             else:
-                r.set(key, msg, ttl)
+                r.hset(f'{self.hkey}:{ts}', mapping=hashdict)
+                r.expire(f'{self.hkey}:{ts}', ttl)
         except Exception as e:
-            logger.error(f'{RedisEvent.logh} Method KO [{e}]')
+            logger.error(f'{self.logh} Method KO [{e}]')
             return None
         else:
-            logger.trace(f'{RedisEvent.logh} Method OK')
+            logger.trace(f'{self.logh} Method OK')
+            return True
+
+    def destroy(self):
+        try:
+            logger.trace(f'{self.logh} Method >> (HASH Destroying)')
+            r.delete(self.hkey)
+        except Exception as e:
+            logger.error(f'{self.logh} Method KO [{e}]')
+            return None
+        else:
+            logger.trace(f'{self.logh} Method OK')
             return True
