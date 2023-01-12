150dcb793d1fdf483829274b9b3038a0ac2cba61
lordslair <5469390+lordslair@users.noreply.github.com>
2023-01-12 14:47:28 +0100

    Removed useless methods

--- nosql/models/RedisCosmetic.py
+++ nosql/models/RedisCosmetic.py
@@ -1,6 +1,5 @@
 # -*- coding: utf8 -*-
 
-import re
 import uuid
 
 from datetime                    import datetime
@@ -8,40 +7,7 @@ from loguru                      import logger
 from redis.commands.search.query import Query
 
 from nosql.connector             import r
-
-
-def str2typed(string):
-    # BOOLEAN False
-    if string == 'False':
-        return False
-    # BOOLEAN True
-    elif string == 'True':
-        return True
-    # None
-    elif string == 'None':
-        return None
-    # Date
-    elif re.match(r'\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}', string):
-        return string
-    # INT
-    elif string.isdigit():
-        return int(string)
-    else:
-        return string
-
-
-def typed2str(string):
-    # None
-    if string is None:
-        return 'None'
-    # BOOLEAN True
-    elif string is True:
-        return 'True'
-    # BOOLEAN False
-    elif string is False:
-        return 'False'
-    else:
-        return string
+from nosql.variables             import str2typed, typed2str
 
 
 class RedisCosmetic:
