ec1d18f59c751bd9ba0ba41895c89b148c5c1637
lordslair <5469390+lordslair@users.noreply.github.com>
2023-01-16 16:27:43 +0100

    Updated PyTests

--- nosql/tests/test_00_redis_config_check.py
+++ nosql/tests/test_00_redis_config_check.py
@@ -8,6 +8,10 @@ LOCAL_PATH = os.path.dirname(os.path.abspath('nosql'))
 sys.path.append(LOCAL_PATH)
 
 from nosql.connector import r  # noqa: E402
+from nosql.initialize import initialize_redis_indexes  # noqa: E402
+
+# We setup some stuff for later
+initialize_redis_indexes()
 
 
 def test_redis_ping():
@@ -21,7 +25,27 @@ def test_redis_config():
     """
     Checking CONFIG:notify-keyspace-events
     """
+    # We start so check&set if necessary (mostly for CI)
+    config = r.config_get(pattern='notify-keyspace-events')
+
+    if config is None or config != '':
+        ret = r.config_set(name='notify-keyspace-events', value='$sxE')
+    assert ret is True
+
+    # We check it again
     config = r.config_get(pattern='notify-keyspace-events')
     assert config is not None
     assert config is not False
     assert config['notify-keyspace-events'] == '$sxE'
+
+
+def test_redis_indexes():
+    """
+    Checking the correctly built indexes from initialize_redis_indexes()
+    """
+    for index in [
+        'user_idx',
+    ]:
+        info = r.ft(index).info()
+        assert info is not None
+        assert info['index_name'] == index
