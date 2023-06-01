dc6a3000f8afb86321ed832bc750b77e316ced1c
lordslair <5469390+lordslair@users.noreply.github.com>
2023-06-01 22:26:25 +0200

    Added better testing

--- ai/tests/test_01_ai_creature.py
+++ ai/tests/test_01_ai_creature.py
@@ -12,20 +12,53 @@ REDIS_PORT = os.environ.get("SEP_BACKEND_REDIS_SVC_SERVICE_PORT", 6379)
 REDIS_DB_NAME = os.environ.get("SEP_REDIS_DB", 0)
 PUBSUB_PATH = os.environ.get('PUBSUB_PATH', 'ai-creature')
 
+INSTANCE_UUID = "00000000-0000-0000-0000-000000000000"
+CREATURE_UUID = "11111111-1111-1111-1111-111111111111"
+CREATURE = {
+    "account": None,
+    "aggro": 0,
+    "created": "2023-06-01 17:03:48",
+    "date": "2023-06-01 17:03:48",
+    "gender": True,
+    "id": CREATURE_UUID,
+    "instance": INSTANCE_UUID,
+    "korp": None,
+    "korp_rank": None,
+    "level": 1,
+    "name": "PyTest Creature",
+    "race": 14,
+    "rarity": "Medium",
+    "squad": None,
+    "squad_rank": None,
+    "targeted_by": None,
+    "x": 100,
+    "xp": 0,
+    "y": 100,
+    }
+INSTANCE = {
+    "creator": CREATURE_UUID,
+    "fast": False,
+    "hardcore": True,
+    "id": INSTANCE_UUID,
+    "map": 1,
+    "public": True,
+    "tick": 3600,
+    }
+
 r = redis.StrictRedis(
-        host=REDIS_HOST,
-        port=REDIS_PORT,
-        db=REDIS_DB_NAME,
-        encoding='utf-8',
-        decode_responses=True,
-        )
+    host=REDIS_HOST,
+    port=REDIS_PORT,
+    db=REDIS_DB_NAME,
+    encoding='utf-8',
+    decode_responses=True,
+    )
 
 
 def test_creature_pop():
     EXPECTED_MSG = {
         "action": 'pop',
-        "instance": 'toto',
-        "creature": 'plop',
+        "instance": INSTANCE,
+        "creature": CREATURE,
         }
     # We subscribe
     pubsub = r.pubsub()
@@ -37,7 +70,7 @@ def test_creature_pop():
     # Should be like this:
     {
         "type":"psubscribe",
-        "pattern":"None",
+        "pattern":None,
         "channel":"*",
         "data":1
         }
@@ -56,8 +89,8 @@ def test_creature_pop():
 def test_creature_kill():
     EXPECTED_MSG = {
         "action": 'kill',
-        "instance": 'toto',
-        "creature": 'plop',
+        "instance": INSTANCE,
+        "creature": CREATURE,
         }
     # We subscribe
     pubsub = r.pubsub()
@@ -69,7 +102,7 @@ def test_creature_kill():
     # Should be like this:
     {
         "type":"psubscribe",
-        "pattern":"None",
+        "pattern":None,
         "channel":"*",
         "data":1
         }
