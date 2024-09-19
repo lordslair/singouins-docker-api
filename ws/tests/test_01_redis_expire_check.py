cecb96f313496ae983911172894c4ab179936cbf
lordslair <5469390+lordslair@users.noreply.github.com>
2024-09-19 11:00:31 +0200

    test: Removed deprecated checks

--- ws/tests/test_01_redis_expire_check.py
+++ ws/tests/test_01_redis_expire_check.py
@@ -40,22 +40,9 @@ def test_redis_expire():
     """
     assert msg['type'] == 'psubscribe'
     assert msg['channel'].decode() == PS_EXPIRE
-    # We SET the data
+    # We SET the KEY
     r.set(FULLKEY, '')
-    # We check the item was properly created
-    """
-    # Should be like this:
-    {
-        "type":"pmessage",
-        "pattern":"*",
-        "channel":"__keyevent@0__:set",
-        "data":"tests:00000000-0000-0000-0000-000000000000:11111111-1111-1111-1111-111111111111:PyTest"
-        }
-    """
-    msg = pubsub.get_message()
-    assert msg['type'] == 'pmessage'
-    assert msg['channel'].decode() == f'__keyevent@{REDIS_BASE}__:set'
-    assert msg['data'].decode() == FULLKEY
+    # We EXPIRE the KEY
     r.expire(FULLKEY, 1)
     # We wait EXPIRE and pubsub doing its job
     time.sleep(3)
