c4fcdb13575a48a7459e38fb003b6c55d1b40825
lordslair <5469390+lordslair@users.noreply.github.com>
2024-09-25 17:42:02 +0200

    fix: DoesNotExist for Creatures

--- discord/subcommands/singouin/_autocomplete.py
+++ discord/subcommands/singouin/_autocomplete.py
@@ -9,16 +9,20 @@ from mongo.models.User import UserDocument
 
 
 async def get_mysingouins_list(ctx: discord.AutocompleteContext):
+    db_list = []
     try:
         User = UserDocument.objects(discord__name=ctx.interaction.user.name).get()
         Creatures = CreatureDocument.objects(account=User.id)
+    except CreatureDocument.DoesNotExist:
+        logger.warning("CreatureDocument Query KO (404)")
+    except UserDocument.DoesNotExist:
+        logger.warning("UserDocument Query KO (404)")
     except Exception as e:
         logger.error(f'MongoDB Query KO [{e}]')
-        return None
     else:
-        db_list = []
         for Creature in Creatures:
             db_list.append(
                 discord.OptionChoice(Creature.name, value=str(Creature.id))
                 )
+    finally:
         return db_list
