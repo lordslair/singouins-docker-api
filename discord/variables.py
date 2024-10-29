bea8a987af567bde6f20f87fa16d061b0de32515
lordslair <5469390+lordslair@users.noreply.github.com>
2024-10-29 17:49:52 +0100

    refactor: Simplified rarity arrays/dicts

--- discord/variables.py
+++ discord/variables.py
@@ -43,55 +43,26 @@ metaNames = {
     'race': [doc.to_mongo().to_dict() for doc in MetaRace.objects()]
 }
 
+# Rarities Emojis, Discord tags, and color integers
+rarity_emoji = ['🟤 ', '⚪ ', '🟢 ', '🔵 ', '🟣 ', '🟠 ']
+rarity_discord = [':brown_square:', ':white_large_square:', ':green_square:', ':blue_square:', ':purple_square:', ':orange_square:']  # noqa: E501
+rarity_integer = [10197915, 16777215, 8311585, 4886754, 9442302, 16098851]
+
+# Rarity dictionnaries for Items
+rarity_item = ['Broken', 'Common', 'Uncommon', 'Rare', 'Epic', 'Legendary']
+rarity_item_types_emoji = dict(zip(rarity_item, rarity_emoji))
+rarity_item_types_discord = dict(zip(rarity_item, rarity_discord))
+rarity_item_types_integer = dict(zip(rarity_item, rarity_integer))
+
+# Rarity dictionnaries for Monsters
+rarity_monster = ['Small', 'Medium', 'Big', 'Unique', 'Boss', 'God']
+rarity_monster_types_emoji = dict(zip(rarity_monster, rarity_emoji))
+rarity_monster_types_discord = dict(zip(rarity_monster, rarity_discord))
+
 item_types_discord = {
     'armor': ':shirt:',
     'weapon': ':dagger:'
     }
-
-rarity_item_types_emoji = {
-    'Broken': '🟤 ',
-    'Common': '⚪ ',
-    'Uncommon': '🟢 ',
-    'Rare': '🔵 ',
-    'Epic': '🟣 ',
-    'Legendary': '🟠 ',
-}
-
-rarity_item_types_discord = {
-    'Broken': ':brown_square:',
-    'Common': ':white_large_square:',
-    'Uncommon': ':green_square:',
-    'Rare': ':blue_square:',
-    'Epic': ':purple_square:',
-    'Legendary': ':orange_square:',
-}
-
-rarity_item_types_integer = {
-    'Broken': 10197915,
-    'Common': 16777215,
-    'Uncommon': 8311585,
-    'Rare': 4886754,
-    'Epic': 9442302,
-    'Legendary': 16098851,
-}
-
-rarity_monster_types_emoji = {
-    'Small': '🟤 ',
-    'Medium': '⚪ ',
-    'Big': '🟢 ',
-    'Unique': '🔵 ',
-    'Boss': '🟣 ',
-    'God': '🟠 ',
-}
-
-rarity_monster_types_discord = {
-    'Small': ':brown_square:',
-    'Medium': ':white_large_square:',
-    'Big': ':green_square:',
-    'Unique': ':blue_square:',
-    'Boss': ':purple_square:',
-    'God': ':orange_square:',
-}
 
 slots_armor = {
     'head': ':military_helmet:',
